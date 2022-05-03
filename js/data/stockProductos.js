355c48284e4ee53764f8260eebe5e6b298def153
unknown <rubendanielconrad@gmail.com>
2022-05-03 19:20:01 -0300

    Se realizan mejoras de SEO, imágenes a webp, og, metadatos

--- js/data/stockProductos.js
+++ js/data/stockProductos.js
@@ -3,42 +3,42 @@ export const stockProductos = [
         id: Math.random(),
         name: "Pan de Jamón",
         desc: "Mide 40cm aproximadamente, tiene como base:<br>400g de masa salada.<br>300g de jamón.<br>100g de tocino.<br>25g de pasas.<br>25g de aceitunas.<br>",
-        img: "../img/Pan_de_Jamón/pan_de_jamon.jpg",
+        img: "../img/Pan_de_Jamón/pan_de_jamon.webp",
         whatsappBtn: "https://api.whatsapp.com/send?phone=+51931064404&text=Hola&nbsp;Mary!&nbsp;Te&nbsp;encargo&nbsp;Pan&nbsp;de&nbsp;Jamón, "
     },
     {
         id: Math.random(),
         name: "Pan 3eme:",
         desc: "Mide 40cm aproximadamente, tiene como base:<br>400g de masa salada.<br>100g de queso fresco.<br>100g de queso de cabra.<br>100g de queso edam.<br>",
-        img: "../img/Pan/pan-3eme.jpg",
+        img: "../img/Pan/pan-3eme.webp",
         whatsappBtn: "https://api.whatsapp.com/send?phone=+51931064404&text=Hola&nbsp;Mary!&nbsp;Te&nbsp;encargo&nbsp;Pan&nbsp;3eme, "
     },
     {
         id: Math.random(),
         name: "Cachitos",
         desc: "Es un pan tamaño individual, que tiene como base:<br>100g de una masa semi dulce.<br>50g de relleno a base de jamón y tocino.<br> Con sus variantes de:<br> Jamón, tocino y queso edam.<br> Jamón, tocino y queso crema.<br> Chocolate. ",
-        img: "../img/Cachitos/cachito_card.jpg",
+        img: "../img/Cachitos/cachito_card.webp",
         whatsappBtn: "https://api.whatsapp.com/send?phone=+51931064404&text=Hola&nbsp;Mary!&nbsp;Te&nbsp;encargo&nbsp;Cachitos,&nbsp; "
     },
     {
         id: Math.random(),
         name: "Golfeados",
         desc: "Es un postre característico de Venezuela, tiene como base:<br>Masa dulce con anís en pepitas.<br>Relleno de papelón (chancaca), queso, anís y azúcar.",
-        img: "../img/Golfeados/golfeado-oscuro.jpg",
+        img: "../img/Golfeados/golfeado-oscuro.webp",
         whatsappBtn: "https://api.whatsapp.com/send?phone=+51931064404&text=Hola&nbsp;Mary!&nbsp;Te&nbsp;encargo&nbsp;Golfeados, "
     },
     {
         id: Math.random(),
         name: "Pan de Guayaba",
         desc: "Es un pan dulce que viene relleno, mide 40 cm aproximadamente, tiene como base:<br>400g de masa dulce.<br>300g de mermelada de guayaba.<br><b>Presentación con queso:</b><br>400g de masa dulce.<br>150g de queso fresco.<br>150g de mermelada de guayaba.",
-        img: "../img/Pan_de_guayaba/pan-de-guayaba.jpg",
+        img: "../img/Pan_de_guayaba/pan-de-guayaba.webp",
         whatsappBtn: "https://api.whatsapp.com/send?phone=+51931064404&text=Hola&nbsp;Mary!&nbsp;Te&nbsp;encargo&nbsp;Pan&nbsp;de&nbsp;Guayaba, "
     },
     {
         id: Math.random(),
         name: "Donas",
         desc: "Postres de tamaño individual, pueden tener diferentes formas (circulares en su mayoría con un circulo en el medio), pueden ser con diferentes toppings, tiene como base:<br>Una masa dulce.<br>Topping de chocolate.<br>Extra de gotas o lluvia de chocolate. ",
-        img: "../img/Donas/dona_blanca.png",
+        img: "../img/Donas/dona_blanca.webp",
         whatsappBtn: "https://api.whatsapp.com/send?phone=+51931064404&text=Hola&nbsp;Mary!&nbsp;Te&nbsp;encargo&nbsp;Donas, "
     },
 ]
