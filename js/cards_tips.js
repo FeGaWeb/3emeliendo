aacd915249d5234b0847999c988aeb314f6fc394
FeGaWeb <andresgalviscamacho21@gmail.com>
2022-05-06 17:59:11 -0500

    se arreglaron las cards de mr tips

--- js/cards_tips.js
+++ js/cards_tips.js
@@ -21,6 +21,27 @@ let productosGenerales=[
         descripcion:"El golfeado es un postre característico de Venezuela, tiene forma cuadrada o circular, se puede parecer al caparazón de un caracol por la forma en la que es cortado, tiene como base: <br> Masa dulce con anís en pepitas.<br> Relleno de papelón (chancaca), queso, anís y azúcar. <br> Es un postre que tamaño individual, se entrega en su presentación por cantidad sobre un papel antiadherente, en bolsa de celofán o cajas y en su presentación individual en bolsas de celofán.",
     },
 
+    {
+        id:"Cachitos",
+        title:"Cachitos",
+        img:"../img/Cachitos/cachito_card.webp",
+        descripcion:"El cachito es un pan tamaño individual, que tiene como base:<br><br> 100 g de una masa semi dulce.<br>50 g de relleno<br><br>Los rellenos pueden variar, en la actualidad contamos con:<br><br>Jamón y tocino.<br>Jamón, tocino y queso crema.<br>Jamón, tocino y queso edam. <br>Chocolate.<br> <br> Se entregan sobre un papel antiadherente dentro de una bolsa de celofán. ",
+    },
+
+    {
+        id:"Pan de guayaba",
+        title:"Pan de guayaba",
+        img:"../img/Pan_de_guayaba/pan-de-guayaba.webp",
+        descripcion:"El pan de guayaba es un pan dulce que viene relleno de una mermelada de guayaba el cual mide 40 cm aproximadamente, tiene como base:<br><br> 400 g de masa dulce. <br> 300 g de mermelada de guayaba.<br><br> Tiene presentación con queso la cual tendría una base de:<br><br> 400 g de masa dulce. <br> 150 g de queso fresco. <br> 150 g de mermelada de guayaba.<br><br> Este producto también lo podemos encontrar en calidad de roll, su presentación y entrega en este caso es igual a la del golfeado y en el respectivo caso del grande, sobre papel antiadherente en una bolsa de celofán.",
+    },
+
+    {
+        id:"Donas",
+        title:"Donas",
+        img:"../img/Donas/dona_blanca.webp",
+        descripcion:"Las donas son postres tamaño individual, que puede tener diferentes formas, circulares en su mayoría con un circulo en el medio, éstas pueden ser con diferentes toppings, tiene como base:<br><br> Una masa dulce.<br> Topping de chocolate.<br> Extra de gotas o lluvia de chocolate. ",
+    },
+
 ]
 
 
