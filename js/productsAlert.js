09b22444fefdc2cf2818238fa9e319da7567b319
unknown <rubendanielconrad@gmail.com>
2022-05-11 18:32:03 -0300

    Se agrega alerta

---
+++ js/productsAlert.js
@@ -0,0 +1,14 @@
+    Swal.fire({
+    title: 'Hola!',
+    text: 'Recuerda que nuestros productos se realizan por encargue y con envío a domicilio.',
+    imageUrl: '../img/logos_3eme/Cachi.png',
+    imageWidth: 200,
+    imageHeight: 200,
+    imageAlt: 'Cachito',
+    background: '#fff',
+    footer: `<a href="mr_tips.html" style="text-decoration: none;">¿Quieres ver nuestros Mr.Tips?</a>`,
+    backdrop: `
+    rgba(96, 163, 217, 0.623)
+  `
+  })
+
